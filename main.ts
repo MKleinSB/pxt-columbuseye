02639483e776db3ff17a154550146c17450aca07
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2020-10-24 17:07:49 +0200

    main.ts gelehrt

--- main.ts
+++ main.ts
@@ -1,12 +1,1 @@
-TCS34725.start()
-TCS34725.LernFarbe(Wasserfarben.Violett)
-TCS34725.LernFarbe(Wasserfarben.Magenta)
-basic.forever(function () {
-    if (TCS34725.Wasserfarbe(Wasserfarben.Violett)) {
-        basic.setLedColor(0x7f00ff)
-    } else if (TCS34725.Wasserfarbe(Wasserfarben.Magenta)) {
-        basic.setLedColor(0xff00ff)
-    } else {
-        basic.setLedColor(0x000000)
-    }
-})
+
